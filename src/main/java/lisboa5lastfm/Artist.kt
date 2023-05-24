6253ec38adcd152d460e3384d3087f5418ea9df1
AxelFontana <axelfontana01@gmail.com>
2023-05-24 19:03:40 -0300

    Added LastFM logo to Artist

--- src/main/java/lisboa5lastfm/Artist.kt
+++ src/main/java/lisboa5lastfm/Artist.kt
@@ -1,6 +1,8 @@
 package lisboa5lastfm
 
 sealed class Artist {
+    val DEFAULT_IMAGE =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png"
     data class ArtistData(
         val artistName: String,
         val artistBioContent: String,
