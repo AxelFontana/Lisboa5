a6ba7ddd0720725783648c61fdd4d928c523ee86
Matias Pippig <matiaspippig@gmail.com>
2023-05-29 16:48:17 -0300

    Agregue la constante con la Url del logo de last fm a la entidad Artist.kt, corregidos errores en ProxyLastFM.kt

--- src/main/java/lisboa5lastfm/Artist.kt
+++ src/main/java/lisboa5lastfm/Artist.kt
@@ -1,8 +1,8 @@
 package lisboa5lastfm
 
+const val LASTFM_LOGO_URL =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png"
 sealed class Artist {
-    val DEFAULT_IMAGE =
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png"
     data class ArtistData(
         val artistName: String,
         val artistBioContent: String,
